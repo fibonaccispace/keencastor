a290a9b080dadbc70c722dca686527097654b485
borkov egor <163824231+fibonaccispace@users.noreply.github.com>
2025-03-22 14:50:36 +0300

    themes

--- front/App.vue
+++ front/App.vue
@@ -9,10 +9,27 @@
 
 <script>
 export default {
+  data() {
+    return {
+      theme: 'light'
+    }
+  },
+  mounted(){
+    this.setTheme()
+  },
   computed: {
     isMain() {
       return this.$route.meta.main || false
     }
+  },
+  methods: {
+    setTheme(){
+      document.body.setAttribute('data-theme', this.theme)
+    },
+    toggleTheme() {
+      this.theme = this.theme === 'dark' ? 'light' : 'dark'
+      this.setTheme()
+    },
   }
 }
 </script>
