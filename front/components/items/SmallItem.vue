f73da7d20af1026ba4c0fefc16afe5fd7f7c44ba
borkov egor <163824231+fibonaccispace@users.noreply.github.com>
2025-07-14 23:16:37 +0300

    SmallItem – kill image preview

--- front/components/items/SmallItem.vue
+++ front/components/items/SmallItem.vue
@@ -2,7 +2,7 @@
   .small-item 
     .content
       .preview
-        img(:src="tempImagePath.high" :style="item.imageStyle")
+        //- img(:src="tempImagePath.high" :style="item.imageStyle")
         .title-wrapper
           .title(v-html="title")
         .description(v-html="description")
